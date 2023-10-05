9aaefc2fcd30bcd2f8d73b92751be17ee34a3796
changwanling <wcha305@aucklanduni.ac.nz>
2023-10-06 11:12:41 +1300

    add Q2

--- question-02/routes/question-two-routes.js
+++ question-02/routes/question-two-routes.js
@@ -6,6 +6,23 @@ const router = express.Router();
 // -------------------------------------------------------------------------
 
 // TODO Your Code Here
+router.get("/", function(req, res) {
+    res.locals.homePage = true;
+    res.locals.title = "Home";
+    res.render("home");
+});
+
+router.get("/hillary", function(req, res) {
+    res.locals.hillaryPage = true;
+    res.locals.title = "Edmund Hillary";
+    res.render("hillary");
+});
+
+router.get("/sheppard", function(req, res) {
+    res.locals.sheppardPage = true;
+    res.locals.title = "Kate Sheppard";
+    res.render("sheppard");
+});
 
 // -------------------------------------------------------------------------
 
